0db86acaf725ace48275238bdcebc1b6cb945e9c
Sharpieman20 <>
2021-11-21 17:59:13 -0600

    Add bisect portal divine comment

--- src/main/java/sharpie/grindsim/agents/nethertravel/divine/BisectPortalDivine.java
+++ src/main/java/sharpie/grindsim/agents/nethertravel/divine/BisectPortalDivine.java
@@ -1,4 +1,9 @@
 package sharpie.grindsim.agents.nethertravel.divine;
 
 public class BisectPortalDivine {
+    // interesting property of portal divine:
+    // if you go directly between two portal divine regions
+    // if the angle is low, then the one to your "right" will be closer
+    // if the angle is high, then the one to your "left" will be closer
+    // towards the middle, selection effects will decrease the angle
 }
